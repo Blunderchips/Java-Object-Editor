2fb9bdfab01b5fc03ff88d9c7fdba800347a556c
Matthew 'siD' Van der Bijl <matthewvdbijl@gmail.com>
2018-09-30 00:05:05 +0200

    Word art on package info.

--- obed/src/main/java/com/github/blunderchips/obed/package-info.java
+++ obed/src/main/java/com/github/blunderchips/obed/package-info.java
@@ -1,3 +1,22 @@
+/*
+ *                   bbbbbbbb                                            dddddddd
+ *      OOOOOOOOO    b::::::b                                            d::::::d
+ *    OO:::::::::OO  b::::::b                                            d::::::d
+ * O O:::::::::::::OOb::::::b                                            d::::::d
+ * O:::::::OOO:::::::Ob:::::b                                            d:::::d
+ * O::::::O   O::::::Ob:::::bbbbbbbbb        eeeeeeeeeeee        ddddddddd:::::d
+ * O:::::O     O:::::Ob::::::::::::::bb    ee::::::::::::ee    dd::::::::::::::d
+ * O:::::O     O:::::Ob::::::::::::::::b  e::::::eeeee:::::ee d::::::::::::::::d
+ * O:::::O     O:::::Ob:::::bbbbb:::::::be::::::e     e:::::ed:::::::ddddd:::::d
+ * O:::::O     O:::::Ob:::::b    b::::::be:::::::eeeee::::::ed::::::d    d:::::d
+ * O:::::O     O:::::Ob:::::b     b:::::be:::::::::::::::::e d:::::d     d:::::d
+ * O:::::O     O:::::Ob:::::b     b:::::be::::::eeeeeeeeeee  d:::::d     d:::::d
+ * O::::::O   O::::::Ob:::::b     b:::::be:::::::e           d:::::d     d:::::d
+ * O:::::::OOO:::::::Ob:::::bbbbbb::::::be::::::::e          d::::::ddddd::::::dd
+ *  OO:::::::::::::OO b::::::::::::::::b  e::::::::eeeeeeee   d:::::::::::::::::d
+ *    OO:::::::::OO   b:::::::::::::::b    ee:::::::::::::e    d:::::::::ddd::::d
+ *      OOOOOOOOO     bbbbbbbbbbbbbbbb       eeeeeeeeeeeeee     ddddddddd   ddddd
+ */
 /**
  * Obed Java object editor.
  *
